08661aa848843f2fc1435d7404edf53a29a58e4e
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-03-23 21:08:30 +0200

    123

---
+++ src/script.js
@@ -0,0 +1,9 @@
+const cars = [];
+
+// task1
+const task1 = cars.filter((car) => car.price > 5000);
+console.table(task1);
+
+// task2
+const task2 = cars.filter((car) => car.color > "green");
+console.table(task1);
