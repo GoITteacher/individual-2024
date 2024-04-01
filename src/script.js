bb607c9222c9d0ba399404a87adafe92b82ba2d0
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-04-01 11:13:55 +0300

    123

--- src/script.js
+++
@@ -1,9 +0,0 @@
-const cars = [];
-
-// task1
-const task1 = cars.filter((car) => car.price > 5000);
-console.table(task1);
-
-// task2
-const task2 = cars.filter((car) => car.color > "green");
-console.table(task1);
