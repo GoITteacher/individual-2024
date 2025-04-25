968a7a3b419285a43f4fbd3e8d62d119fa50af96
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-04-25 22:21:28 +0300

    123

---
+++ src/example/example.js
@@ -0,0 +1,233 @@
+const cars = [
+  {
+    id: 1,
+    title: 'Toyota Camry',
+    price: 28312,
+    img: 'https://picsum.photos/720?random=1&car,toyota',
+    onSale: false,
+    type: 'supercar',
+  },
+  {
+    id: 2,
+    title: 'Honda Civic',
+    price: 49756,
+    img: 'https://picsum.photos/720?random=2&car,Honda',
+    onSale: true,
+    type: 'suv',
+  },
+  {
+    id: 3,
+    title: 'BMW X5',
+    price: 37689,
+    img: 'https://picsum.photos/720?random=3&car,BMW',
+    onSale: true,
+    type: 'supercar',
+  },
+  {
+    id: 4,
+    title: 'Chevrolet Malibu',
+    price: 40233,
+    img: 'https://picsum.photos/720?random=4&car,Chevrolet',
+    onSale: false,
+    type: 'suv',
+  },
+  {
+    id: 5,
+    title: 'Mercedes-Benz E-Class',
+    price: 19258,
+    img: 'https://picsum.photos/720?random=5&car,Mercedes-Benz',
+    onSale: true,
+    type: 'sedan',
+  },
+  {
+    id: 6,
+    title: 'Audi A4',
+    price: 31674,
+    img: 'https://picsum.photos/720?random=6&car,Audi',
+    onSale: false,
+    type: 'sedan',
+  },
+  {
+    id: 7,
+    title: 'Lexus RX',
+    price: 48326,
+    img: 'https://picsum.photos/720?random=7&car,Lexus',
+    type: 'supercar',
+    onSale: false,
+  },
+  {
+    id: 8,
+    title: 'Nissan Altima',
+    price: 36890,
+    img: 'https://picsum.photos/720?random=8&car,Nissan',
+    onSale: true,
+    type: 'suv',
+  },
+  {
+    id: 9,
+    title: 'Hyundai Sonata',
+    price: 29147,
+    img: 'https://picsum.photos/720?random=9&car,Hyundai',
+    onSale: false,
+    type: 'suv',
+  },
+  {
+    id: 10,
+    title: 'Ford Mustang',
+    price: 43751,
+    img: 'https://picsum.photos/720?random=10&car,Ford',
+    type: 'supercar',
+    onSale: true,
+  },
+  {
+    id: 11,
+    title: 'Toyota Corolla',
+    price: 41872,
+    img: 'https://picsum.photos/720?random=11&car,Toyota',
+    onSale: false,
+    type: 'sedan',
+  },
+  {
+    id: 12,
+    title: 'Honda Accord',
+    price: 35491,
+    img: 'https://picsum.photos/720?random=12&car,Honda',
+    onSale: true,
+    type: 'sedan',
+  },
+  {
+    id: 13,
+    title: 'BMW 3 Series',
+    price: 26783,
+    img: 'https://picsum.photos/720?random=13&car,BMW',
+    type: 'supercar',
+    onSale: true,
+  },
+  {
+    id: 14,
+    title: 'Chevrolet Silverado',
+    price: 49205,
+    img: 'https://picsum.photos/720?random=14&car,Chevrolet',
+    onSale: false,
+    type: 'suv',
+  },
+  {
+    id: 15,
+    title: 'Mercedes-Benz GLC',
+    price: 38957,
+    img: 'https://picsum.photos/720?random=15&car,Mercedes-Benz',
+    onSale: true,
+    type: 'sedan',
+  },
+  {
+    id: 16,
+    title: 'Audi Q5',
+    price: 43296,
+    img: 'https://picsum.photos/720?random=16&car,Audi',
+    type: 'supercar',
+    onSale: false,
+  },
+  {
+    id: 17,
+    title: 'Lexus IS',
+    price: 31548,
+    img: 'https://picsum.photos/720?random=17&car,Lexus',
+    onSale: false,
+    type: 'suv',
+  },
+  {
+    id: 18,
+    title: 'Nissan Rogue',
+    price: 27041,
+    img: 'https://picsum.photos/720?random=18&car,Nissan',
+    onSale: true,
+    type: 'sedan',
+  },
+  {
+    id: 19,
+    title: 'Hyundai Tucson',
+    price: 44103,
+    img: 'https://picsum.photos/720?random=19&car,Hyundai',
+    onSale: false,
+    type: 'suv',
+  },
+  {
+    id: 20,
+    title: 'Ford Explorer',
+    price: 37619,
+    img: 'https://picsum.photos/720?random=20&car,Ford',
+    type: 'supercar',
+    onSale: true,
+  },
+];
+
+const refs = {
+  createForm: document.querySelector('.js-create-form'),
+  searchForm: document.querySelector('.js-form'),
+  container: document.querySelector('.js-list'),
+};
+
+//!======================================================
+
+refs.createForm.addEventListener('submit', handleCreateFormSubmit);
+
+function handleCreateFormSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(refs.createForm);
+  const userData = Object.fromEntries(formData);
+
+  userData.id = Date.now();
+  userData.onSale = userData.onSale ? true : false;
+  userData.price *= 1;
+
+  const carMarkup = carTemplate(userData);
+  refs.container.insertAdjacentHTML('afterbegin', carMarkup);
+
+  cars.unshift(userData);
+  refs.createForm.reset();
+}
+
+//!======================================================
+
+function carTemplate(car) {
+  const onSaleClass = car.onSale ? 'on-sale' : '';
+
+  return `<li class="car-item ${onSaleClass}">
+        <img src="${car.img}" alt="${car.title}" />
+        <div class="car-info">
+          <h3>${car.title}</h3>
+          <p>Type: ${car.type}</p>
+          <p>Price: ${car.price}$</p>
+        </div>
+      </li>`;
+}
+
+function carsTemplate(cars) {
+  return cars.map(carTemplate).join('');
+}
+
+function renderCars(cars) {
+  const markup = carsTemplate(cars);
+  refs.container.innerHTML = markup;
+}
+
+renderCars(cars);
+//!======================================================
+
+refs.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const userTitle = refs.searchForm.elements.query.value;
+  const userType = refs.searchForm.elements.type.value;
+
+  const filteredCars = cars.filter(el => {
+    const isValidTitle = el.title.includes(userTitle);
+    const isValidType = el.type === userType;
+    const isAllType = userType === 'all';
+    return isValidTitle && (isValidType || isAllType);
+  });
+
+  renderCars(filteredCars);
+});
+
+//!======================================================
