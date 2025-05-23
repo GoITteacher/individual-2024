a67c1f39d39fca1263db0a031ed0c1abc290b884
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-05-24 00:07:10 +0300

    123

---
+++ src/js/task-1.js
@@ -0,0 +1,2 @@
+//https://food-boutique.b.goit.study/api-docs/#/Products/get_products
+//https://www.figma.com/design/drnNO3WGWNnZHq7P43qNR0/FoodBoutique-%F0%9F%A5%A6%F0%9F%8D%93?node-id=0-1&p=f&t=DZSYJJ8LrzpOpx7L-0
